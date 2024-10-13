9f20b58428948ee4a61689b49cf83e2172627286
William Heuer <williamfheuer@gmail.com>
2024-10-12 22:18:25 -0500

    [Networking] Save status before full wifi manager and mDNS incorporation

--- Firmware/LED-cube/main/Network/network.c
+++ Firmware/LED-cube/main/Network/network.c
@@ -13,22 +13,23 @@ static const char *TAG = "[NETWORK]";
 void networkLaunch(void)
 {
     // Boilerplate network stack initilization
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // ESP_ERROR_CHECK(nvs_flash_init());
+    // ESP_ERROR_CHECK(esp_netif_init());
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     // Attempt connection to WiFi
     if (wifiConnect() == ESP_OK)
     {
+
         // Connected to WiFi, we can start our http webserver
-        if (startServer() == ESP_OK)
-        {
-            ESP_LOGI(TAG, "HTTP server started");
-        }
-        else
-        {
-            ESP_LOGE(TAG, "Failed to start HTTP server");
-        }
+        // if (startServer() == ESP_OK)
+        // {
+        //     ESP_LOGI(TAG, "HTTP server started");
+        // }
+        // else
+        // {
+        //     ESP_LOGE(TAG, "Failed to start HTTP server");
+        // }
     }
     else
     {
